c74def3575a43ea5dfb29e7203d8daf9e383089c
SquareScreamYT <kohkaifeng06@gmail.com>
2024-11-16 14:10:09 +0800

    cyan

--- drawos.js
+++ drawos.js
@@ -30,7 +30,9 @@ function draw() {
   if (backgroundSprite) { drawSprite(0, 0, backgroundSprite); }
 
   // menu bar
-  drawRect(0, 0, 255, 9, "#495057", true);
+  // for black
+  // drawRect(0, 0, 255, 9, "#495057", true);
+  drawRect(0, 0, 255, 9, "#0b7285", true);
   // logo
   if (cursorSprite) { drawSprite(1, 1, logoSprite); }
   // time & date
