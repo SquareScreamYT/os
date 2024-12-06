dbb857c5569fd09890db97408616b7dced131fea
SquareScreamYT <kohkaifeng06@gmail.com>
2024-12-06 17:09:09 +0800

    guh

---
+++ palette.js
@@ -0,0 +1,128 @@
+opencolor = {
+  "white": "#ffffff",
+  "black": "#000000",
+  "gray": [
+    "#f8f9fa", "#f1f3f5", "#e9ecef", "#dee2e6", "#ced4da",
+    "#adb5bd", "#868e96", "#495057", "#343a40", "#212529"
+  ],
+  "red": [
+    "#fff5f5", "#ffe3e3", "#ffc9c9", "#ffa8a8", "#ff8787",
+    "#ff6b6b", "#fa5252", "#f03e3e", "#e03131", "#c92a2a"
+  ],
+  "pink": [
+    "#fff0f6", "#ffdeeb", "#fcc2d7", "#faa2c1", "#f783ac",
+    "#f06595", "#e64980", "#d6336c", "#c2255c", "#a61e4d"
+  ],
+  "grape": [
+    "#f8f0fc", "#f3d9fa", "#eebefa", "#e599f7", "#da77f2",
+    "#cc5de8", "#be4bdb", "#ae3ec9", "#9c36b5", "#862e9c"
+  ],
+  "violet": [
+    "#f3f0ff", "#e5dbff", "#d0bfff", "#b197fc", "#9775fa",
+    "#845ef7", "#7950f2", "#7048e8", "#6741d9", "#5f3dc4"
+  ],
+  "indigo": [
+    "#edf2ff", "#dbe4ff", "#bac8ff", "#91a7ff", "#748ffc",
+    "#5c7cfa", "#4c6ef5", "#4263eb", "#3b5bdb", "#364fc7"
+  ],
+  "blue": [
+    "#e7f5ff", "#d0ebff", "#a5d8ff", "#74c0fc", "#4dabf7",
+    "#339af0", "#228be6", "#1c7ed6", "#1971c2", "#1864ab"
+  ],
+  "cyan": [
+    "#e3fafc", "#c5f6fa", "#99e9f2", "#66d9e8", "#3bc9db",
+    "#22b8cf", "#15aabf", "#1098ad", "#0c8599", "#0b7285"
+  ],
+  "teal": [
+    "#e6fcf5", "#c3fae8", "#96f2d7", "#63e6be", "#38d9a9",
+    "#20c997", "#12b886", "#0ca678", "#099268", "#087f5b"
+  ],
+  "green": [
+    "#ebfbee", "#d3f9d8", "#b2f2bb", "#8ce99a", "#69db7c",
+    "#51cf66", "#40c057", "#37b24d", "#2f9e44", "#2b8a3e"
+  ],
+  "lime": [
+    "#f4fce3", "#e9fac8", "#d8f5a2", "#c0eb75", "#a9e34b",
+    "#94d82d", "#82c91e", "#74b816", "#66a80f", "#5c940d"
+  ],
+  "yellow": [
+    "#fff9db", "#fff3bf", "#ffec99", "#ffe066", "#ffd43b",
+    "#fcc419", "#fab005", "#f59f00", "#f08c00", "#e67700"
+  ],
+  "orange": [
+    "#fff4e6", "#ffe8cc", "#ffd8a8", "#ffc078", "#ffa94d",
+    "#ff922b", "#fd7e14", "#f76707", "#e8590c", "#d9480f"
+  ],
+  "brown": [
+    "#fdf0e4", "#f8dcc7", "#f1c7ac", "#e4ad8f", "#cc9477",
+    "#b57a5e", "#9a6548", "#7b4e36", "#5e3b2b", "#3e261a"
+  ]
+}
+
+// resurrect 64 by kerrie lake
+const simplifiedColors = {
+  black: "#2e222f",
+  darkgray: "#3e3546",
+  purple: "#625565",
+  rose: "#966c6c",
+  lightbrown: "#ab947a",
+  plum: "#694f62",
+  slate: "#7f708a",
+  coolgray: "#9babb2",
+  mint: "#c7dcd0",
+  white: "#ffffff",
+  darkred: "#6e2727",
+  crimson: "#b33831",
+  coral: "#ea4f36",
+  orange: "#f57d4a",
+  ruby: "#ae2334",
+  scarlet: "#e83b3b",
+  tangerine: "#fb6b1d",
+  amber: "#f79617",
+  yellow: "#f9c22b",
+  wine: "#7a3045",
+  copper: "#9e4539",
+  burntsienna: "#cd683d",
+  gold: "#e6904e",
+  lemon: "#fbb954",
+  olive: "#4c3e24",
+  moss: "#676633",
+  olivegreen: "#a2a947",
+  chartreuse: "#d5e04b",
+  paleyellow: "#fbff86",
+  darkteal: "#165a4c",
+  forestgreen: "#239063",
+  seafoamgreen: "#1ebc73",
+  limegreen: "#91db69",
+  pastelgreen: "#cddf6c",
+  charcoalgray: "#313638",
+  tealgray: "#374e4a",
+  mutedteal: "#547e64",
+  sage: "#92a984",
+  olivegray: "#b2ba90",
+  oceanblue: "#0b5e65",
+  turquoise: "#0b8a8f",
+  mintgreen: "#0eaf9b",
+  aquamarine: "#30e1b9",
+  lightturquoise: "#8ff8e2",
+  midnightblue: "#323353",
+  indigo: "#484a77",
+  royalblue: "#4d65b4",
+  cyan: "#4d9be6",
+  babyblue: "#8fd3ff",
+  deepplum: "#45293f",
+  darkviolet: "#6b3e75",
+  lavender: "#905ea9",
+  lightpurple: "#a884f3",
+  palepink: "#eaaded",
+  magenta: "#753c54",
+  fuchsia: "#a24b6f",
+  rosepink: "#cf657f",
+  softpink: "#ed8099",
+  berryred: "#831c5d",
+  raspberry: "#c32454",
+  flamingopink: "#f04f78",
+  blushpink: "#f68181",
+  peach: "#fca790",
+  beige: "#fdcbb0"
+};
